b275c6bff68497e6a47d29b819938188e9043569
ErnaneMoraes <ernanehelio@gmail.com>
2025-05-25 20:40:08 -0300

    Update api.js

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,2 +1,3 @@
 //export const API_URL = "https://backendaraleve01-486376460769.southamerica-east1.run.app";
-export const API_URL = "http://localhost:8080";
+//export const API_URL = "http://localhost:8080";
+export const API_URL = "https://araleve-dev-486376460769.southamerica-east1.run.app";
