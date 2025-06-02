6791bce90850be489ed2a235ccf34dc2c3a7c347
ErnaneMoraes <ernanehelio@gmail.com>
2025-06-02 19:07:19 -0300

    Update api.js

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,4 +1,4 @@
-//export const API_URL = "https://backendaraleve01-486376460769.southamerica-east1.run.app";
-export const API_URL = "http://localhost:8080";
-//export const API_URL = "https://araleve-dev-486376460769.southamerica-east1.run.app";
-//export const API_URL = "https://sistema-araleve.onrender.com";
+//export const API_URL = "https://backendaraleve01-486376460769.southamerica-east1.run.app"; // API google PROD
+//export const API_URL = "http://localhost:8080"; // API local
+//export const API_URL = "https://araleve-dev-486376460769.southamerica-east1.run.app"; // API google DEV
+export const API_URL = "https://sistema-araleve.onrender.com"; //API ATUAL PROD
